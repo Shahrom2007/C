01f65a7d673f8a8f7f5fe2f2266014ae91d87216
Shahrom2007 <sneiksodiev@gmail.com>
2026-03-31 18:14:47 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include "geometry.h"
 
-
 int is_triangle(double a, double b, double c) {
-
     if (a <= 0 || b <= 0 || c <= 0) {
         return 0;
     }
-
     if ((a + b > c) && (a + c > b) && (b + c > a)) {
         return 1;
     }
@@ -17,30 +14,28 @@ int is_triangle(double a, double b, double c) {
 int main() {
     double side1, side2, side3;
     int result;
+    char test;
     
-    printf("A=? B=? C=? \n");
-    
-
-  
+    printf("A=? B=? C=?\n ");
     result = scanf("%lf %lf %lf", &side1, &side2, &side3);
     
 
+    test = getchar();
+    
     if (result != 3) {
-        printf("Error: you need to enter three numbers\n");
-        return 1;
-    }
-    
-
-    if (!is_triangle(side1, side2, side3)) {
-        printf("Error: a triangle with such sides does not exist!\n");
-        
-        return 1;
-    }
-    
+        printf("Error: You need to enter three numbers!\n");
+    } else if (test != '\n' && test != EOF) {
   
-    printf("\nCalculation results:\n");
-    printf("P: %.2f\n", get_perimeter(side1, side2, side3));
-    printf("S:  %.2f\n", get_area(side1, side2, side3));
+        printf("Error you entered more than 3 numbers!\n");
+
+        while (getchar() != '\n');
+    } else if (!is_triangle(side1, side2, side3)) {
+        printf("Error: A triangle with such sides does not exist!\n");
+    } else {
+        printf("\nCalculation results:\n");
+        printf("P: %.2f\n", get_perimeter(side1, side2, side3));
+        printf("S: %.2f\n", get_area(side1, side2, side3));
+    }
     
     return 0;
 }
