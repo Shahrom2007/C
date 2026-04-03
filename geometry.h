0dd949b4b5aee989667d97d753920fb7d594ce7d
Shahrom2007 <sneiksodiev@gmail.com>
2026-04-04 09:53:36 +1000

    Update geometry.h

--- geometry.h
+++ geometry.h
@@ -1,8 +1,8 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
-
+#include <stdbool.h>
 
 double get_perimeter(double a, double b, double c);
 double get_area(double a, double b, double c);
-
+bool is_triangle_vaid(double a, double b, double c);
 #endif
