76fbddb0e98dae6644ea16d4d69fad8980109eff
Shahrom2007 <sneiksodiev@gmail.com>
2026-04-04 09:54:08 +1000

    Update geometry.c

--- geometry.c
+++ geometry.c
@@ -1,6 +1,16 @@
 #include "geometry.h"
 #include <math.h>
+#include <stdbool.h>
 
+ bool is_triangle_vaid(double a, double b, double c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      
+        return false;
+    }
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        return true;
+    }
+}
 
 double get_perimeter(double a, double b, double c) {
     return a + b + c;
