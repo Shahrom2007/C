66ed041e50459a61c32444062681075e47707d52
Shahrom2007 <sneiksodiev@gmail.com>
2026-04-10 23:01:12 +1000

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+double** add(int size, double **m1, double **m2);
+double** subtract(int size, double **m1, double **m2);
+double** multiply(int size, double **m1, double **m2);
+
+#endif
